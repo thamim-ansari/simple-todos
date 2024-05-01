69f39b4a3116c76bb8d67901fd8260ac365e196a
thamim-ansari <thamimansari0117@gmail.com>
2024-05-01 12:52:13 +0000

    added checkbox,edit and save exisiting todo and add new todo functionality

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,4 +1,5 @@
 import {Component} from 'react'
+import {v4 as uid} from 'uuid'
 import TodoItem from '../TodoItem'
 import './index.css'
 
@@ -6,40 +7,49 @@ const initialTodosList = [
   {
     id: 1,
     title: 'Book the ticket for today evening',
+    isChecked: false,
   },
   {
     id: 2,
     title: 'Rent the movie for tomorrow movie night',
+    isChecked: false,
   },
   {
     id: 3,
     title: 'Confirm the slot for the yoga session tomorrow morning',
+    isChecked: false,
   },
   {
     id: 4,
     title: 'Drop the parcel at Bloomingdale',
+    isChecked: false,
   },
   {
     id: 5,
     title: 'Order fruits on Big Basket',
+    isChecked: false,
   },
   {
     id: 6,
     title: 'Fix the production issue',
+    isChecked: false,
   },
   {
     id: 7,
     title: 'Confirm my slot for Saturday Night',
+    isChecked: false,
   },
   {
     id: 8,
     title: 'Get essentials for Sunday car wash',
+    isChecked: false,
   },
 ]
 
 class SimpleTodos extends Component {
   state = {
     todosList: initialTodosList,
+    todosInput: '',
   }
 
   deleteTodo = id => {
@@ -51,18 +61,72 @@ class SimpleTodos extends Component {
     })
   }
 
+  checkTodo = id => {
+    this.setState(prev => ({
+      todosList: prev.todosList.map(eachItem => {
+        if (eachItem.id === id) {
+          return {...eachItem, isChecked: !eachItem.isChecked}
+        }
+        return eachItem
+      }),
+    }))
+  }
+
+  onChangeTodo = event => {
+    this.setState({todosInput: event.target.value})
+  }
+
+  onEditTodo = ({id, textInput}) => {
+    console.log(textInput)
+    this.setState(prev => ({
+      todosList: prev.todosList.map(eachItem => {
+        if (eachItem.id === id) {
+          return {...eachItem, title: textInput}
+        }
+        return eachItem
+      }),
+    }))
+  }
+
+  onClickAddTodos = () => {
+    const {todosInput} = this.state
+    const newTodo = {
+      id: uid(),
+      title: todosInput,
+      isChecked: false,
+    }
+    this.setState(prev => ({
+      todosList: [...prev.todosList, newTodo],
+      todosInput: '',
+    }))
+  }
+
   render() {
-    const {todosList} = this.state
+    const {todosList, todosInput} = this.state
+    console.log(todosList)
+
     return (
       <div className="todo-app">
         <div className="todo-container">
           <h1 className="heading">Simple Todos</h1>
+          <div>
+            <input
+              type="text"
+              value={todosInput}
+              onChange={this.onChangeTodo}
+            />
+            <button type="button" onClick={this.onClickAddTodos}>
+              Add
+            </button>
+          </div>
           <ul className="todos-list">
             {todosList.map(eachTodo => (
               <TodoItem
                 key={eachTodo.id}
                 todoDetails={eachTodo}
                 deleteTodo={this.deleteTodo}
+                checkTodo={this.checkTodo}
+                onEditTodo={this.onEditTodo}
               />
             ))}
           </ul>
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,16 +1,52 @@
+import {useState} from 'react'
 import './index.css'
 
 const TodoItem = props => {
-  const {todoDetails, deleteTodo} = props
-  const {id, title} = todoDetails
+  const [isEditActive, setEdit] = useState(false)
+  const [isSaveActive, setSave] = useState(false)
+  const {todoDetails, deleteTodo, checkTodo, onEditTodo} = props
+  const {id, title, isChecked} = todoDetails
+  const [textInput, setTextInput] = useState(title)
+  const toDoClass = isChecked ? 'title checked' : 'title'
 
   const onDeleteTodo = () => {
     deleteTodo(id)
   }
 
+  const onClickCheck = () => {
+    checkTodo(id)
+  }
+
+  const onClickEdit = () => {
+    setEdit(true)
+    setSave(true)
+  }
+
+  const onChangeTodo = event => {
+    setTextInput(event.target.value)
+  }
+  const onClickSave = () => {
+    setEdit(false)
+    setSave(false)
+    onEditTodo({id, textInput})
+  }
   return (
     <li className="todo-item">
-      <p className="title">{title}</p>
+      <input type="checkbox" onClick={onClickCheck} />
+      {isEditActive ? (
+        <input type="text" value={textInput} onChange={onChangeTodo} />
+      ) : (
+        <p className={toDoClass}>{title}</p>
+      )}
+      {isSaveActive ? (
+        <button type="button" onClick={onClickSave}>
+          save
+        </button>
+      ) : (
+        <button type="button" onClick={onClickEdit}>
+          Edit
+        </button>
+      )}
       <button type="button" className="delete-btn" onClick={onDeleteTodo}>
         Delete
       </button>
